407820eb022a2f9aa30418af5753c0c7fb72d8ad
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-12-28 11:35:07 -0300

    Adicionar arquivos de projeto.

---
+++ Encapsulamento1-csharp/ContaBancaria.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento1_csharp
+{
+    internal class ContaBancaria
+    {
+        public string Conta { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+
+        public ContaBancaria(){}
+
+        public ContaBancaria(string conta, string titular, double saldo)
+        {
+            Conta = conta;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            
+            Saldo -= quantia + 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Conta
+                + ", Titular: "
+                + Titular
+                + ", Saldo: "
+                + Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Encapsulamento1-csharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Encapsulamento1_csharp;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta = new ContaBancaria();
+
+            Console.Write("Entre o número da conta: ");
+            conta.Conta = Console.ReadLine();
+            Console.Write("Entre o titular da conta: ");
+            conta.Titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n): ");
+            char isDeposito = char.Parse(Console.ReadLine());   
+            if(isDeposito == 's')
+            {
+                Console.Write("Entre com o valor do depósito: ");
+                conta.Saldo = double.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine(conta);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+
+            Console.Write("Entre com valor de depósito: ");
+            conta.Deposito(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+
+            Console.Write("Entre com valor para saque: ");
+            conta.Saque(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
